043e99d0b12269c1cb40d9681279996e1d06273f
alexander <alexanderglover478@gmail.com>
2025-04-09 09:38:37 +0000

    implementing-elasticache-19

--- src/routes/v1/home.routes.ts
+++ src/routes/v1/home.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { homePage, postToDb, redirectToLongUrl } from "../../controller/home.controller";
-import { cacheMiddleware } from "../../middleware/cache.middleware";
+// import { cacheMiddleware } from "../../middleware/cache.middleware";
 
 const router = Router();
 
 router.get("/", homePage);
 router.post("/", postToDb);
-router.get("/:shortId", cacheMiddleware(3600), redirectToLongUrl);
+router.get("/:shortId", redirectToLongUrl);
  
 export default router;
